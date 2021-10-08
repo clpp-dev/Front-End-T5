86cef698d838cbd7cdeb7ba809be28eabe774041
clperez029 <88810234+clperez029@users.noreply.github.com>
2021-10-08 15:47:11 -0500

    Se sube a repositorio actividad de votacion con los parametros solicitados

---
+++ Taller1-VOTAR/js/app.js
@@ -0,0 +1,64 @@
+const nombre = document.querySelector("#nombre");
+const edad = document.querySelector("#edad");
+const boton = document.querySelector("#btn-enviar");
+const mensaje = document.querySelector("#mensaje");
+const fecNac = document.querySelector("#fecnac")
+const iconoexito = document.querySelector("#iconoexito")
+const iconoerror = document.querySelector("#iconoerror")
+
+boton.addEventListener('click', ValidarVoto)
+edad.addEventListener('keydown', TeclaEnter)
+fecNac.addEventListener('change', CalcularConFechaNac)
+
+function ValidarVoto(){
+    let nomUsuario= nombre.value;
+    let edadUsuario= edad.value;
+    if (edadUsuario > 17){
+        mensaje.textContent  = `Señor ${nomUsuario}, Puede votar su edad es ${edadUsuario}`
+    }
+    else{
+        mensaje.textContent  = `Señor ${nomUsuario}, No puede votar su edad es ${edadUsuario}`
+    }
+    
+}
+
+function TeclaEnter(e){
+    tecla = e.keyCode
+    if (tecla == 13){
+        ValidarVoto();
+    }
+}
+
+function CalcularConFechaNac(e){
+    e.preventDefault()
+    let fechaNacUsuario = fecnac.value;
+    fechaNacUsuario = fechaNacUsuario.split("-")
+
+    let date = new Date()
+    let diaActual = date.getDate()
+    let mesActual = date.getMonth()
+    let annioActual =  date.getFullYear()
+
+    let diasUsuario =  diaActual - fechaNacUsuario[1]
+    let mesUsuario =  mesActual - fechaNacUsuario[1]
+    let anniosUsuario = annioActual - fechaNacUsuario[0]
+
+    if (anniosUsuario >= 18){
+        edad.setAttribute('value', `${anniosUsuario}`)
+        iconoerror.classList.remove('activo')
+        iconoerror.classList.add('oculto')
+        iconoexito.classList.remove('oculto')
+        iconoexito.classList.add('activo')
+        ValidarVoto();
+    }
+    else{
+        edad.setAttribute('value', `${anniosUsuario}`)
+        iconoexito.classList.remove('activo')
+        iconoexito.classList.add('oculto')
+        iconoerror.classList.remove('oculto')
+        iconoerror.classList.add('activo')
+        ValidarVoto();
+    }
+    console.log("Dias: " + diasUsuario,"Meses: " + mesUsuario, "annios" + anniosUsuario) // Este Console Log es para saber si estan bien los parametros de la fecha
+}
+
